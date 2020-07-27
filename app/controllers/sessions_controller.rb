10fa3e54c384886aff2315b11d0a5f716306f23e
zchesny <z.chesny@gmail.com>
2020-07-26 19:01:27 -0700

    Added login option for facebook or regular

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,22 +6,25 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-        @user = User.find_or_create_by(name: auth['info']['name']) do |u|
-            u.name = auth['info']['name']
-            u.role = 'Teacher'
-            u.password = 'pass'
-        end 
         binding.pry
-        session[:user_id] = @user.id
-        redirect_to user_path(current_user)
-        # user = User.find_by(name: params[:name])
-        # if user && user.authenticate(params[:password])
-        #     session[:user_id] = user.id
-        #     redirect_to user_path(current_user) 
-        # else # show form again
-        #     flash[:alert] = params[:name]
-        #     redirect_to login_path, notice: "Sorry, we could not find a user with a matching name and/or password."
-        # end 
+        if auth
+            @user = User.find_or_create_by(name: auth['info']['name']) do |u|
+                u.name = auth['info']['name']
+                u.role = 'Student'
+                u.password = 'pass'
+            end 
+            session[:user_id] = @user.id
+            redirect_to user_path(current_user)
+        else 
+            user = User.find_by(name: params[:name])
+            if user && user.authenticate(params[:password])
+                session[:user_id] = user.id
+                redirect_to user_path(current_user) 
+            else # show form again
+                flash[:alert] = params[:name]
+                redirect_to login_path, notice: "Sorry, we could not find a user with a matching name and/or password."
+            end 
+        end 
     end 
 
     def destroy
