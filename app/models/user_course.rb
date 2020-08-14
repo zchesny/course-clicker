0d96e2dce304a51980a79a028a8360bb080e07c6
zchesny <z.chesny@gmail.com>
2020-08-13 17:43:23 -0700

    grading option works

--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -1,9 +1,7 @@
 class UserCourse < ApplicationRecord
     belongs_to :user 
     belongs_to :course
-<<<<<<< HEAD
 
-<<<<<<< HEAD
     def self.find_by_user_course(user_id, course_id)
         UserCourse.where("user_id = ? AND course_id = ?", user_id, course_id).first
     end
@@ -11,20 +9,4 @@ class UserCourse < ApplicationRecord
     def self.get_grade(user_id, course_id)
         UserCourse.find_by_user_course(user_id, course_id).grade
     end 
-=======
-    def self.set_user_courses_date(user_ids, course_id, date)
-        binding.pry
-        user_ids.each do |user_id|
-            user_course = UserCourse.find_by_user_course(user_id, course_id)
-            user_course.date = date
-            user_course.save 
-        end 
-    end
-
-    def self.find_by_user_course(user_id, course_id)
-        UserCourse.where("user_id = ? AND course_id = ?", user_id, course_id)
-    end
->>>>>>> df7056d... nonworking date with enrollment
-=======
->>>>>>> 3cc0c79... deleting stuff for date in enrollment
 end
